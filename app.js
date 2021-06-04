8551c87ba55a22f7eeb23272fbc1e835882e8daf
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-04 10:40:31 -0700

    tracking out twitter keywords for bot

--- app.js
+++ app.js
@@ -6,4 +6,23 @@ const twitterSetup = new Twit({
     consumer_secret: process.env.API_Secret_Key,
     access_token: process.env.Access_Token,
     access_token_secret: process.env.Access_Token_Secret
-})
+});
+
+//starting stream and tracking keywords
+const stream = twitterSetup.stream('statuses/filter', { track: '#DCI2021, #marchingband' });
+
+// use this to log errors from requests
+
+function responseCallback(err, data, response) {
+    console.log(err);
+}
+
+//event handler
+stream.on('tweet', tweet => {
+    //retweet
+    twitterSetup.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+
+    //like tweet
+    twitterSetup.post('favorites/create', { id: tweet.id_str }, responseCallback);
+});
+
