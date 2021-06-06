93cf15f4d593e5347f3d35e818ff217739b3723f
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-05 21:23:19 -0700

    testing second tweet by bot

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ let twitterSetup = new Twit(config)
 
 // testing  tweet 'hello world!'
 
-twitterSetup.post('statuses/update', { status: 'hello world!' }, function (err, data, response) {
+twitterSetup.post('statuses/update', { status: 'Looking forward to DCI performances' }, function (err, data, response) {
     console.log(data)
 })
+
+//starting stream and tracking keywords
+const stream = twitterSetup.stream('statuses/filter', { track: '#DCI2021, #marchingband' });
+
+// use this to log errors from requests
+
+function responseCallback(err, data, response) {
+    console.log(err);
+}
+
+//event handler
+stream.on('tweet', tweet => {
+    //retweet
+    twitterSetup.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+
+    //like tweet
+    twitterSetup.post('favorites/create', { id: tweet.id_str }, responseCallback);
+});
 
