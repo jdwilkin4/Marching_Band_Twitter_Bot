9b082d149f687976aba58f9bde45c3e5e8ff7b0a
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-05 21:03:32 -0700

    testing hello world post

--- app.js
+++ app.js
@@ -3,21 +3,10 @@ const config = require('./config');
 
 const twitterSetup = new Twit(config)
 
-//starting stream and tracking keywords
-const stream = twitterSetup.stream('statuses/filter', { track: ' #marchingband' });
 
-// use this to log errors from requests
+// testing  tweet 'hello world!'
 
-function responseCallback(err, data, response) {
-    console.log(err);
-}
-
-//event handler
-stream.on('tweet', tweet => {
-    //retweet
-    twitterSetup.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-
-    //like tweet
-    twitterSetup.post('favorites/create', { id: tweet.id_str }, responseCallback);
-});
+twitterSetup.post('statuses/update', { status: 'hello world!' }, function (err, data, response) {
+    console.log(data)
+})
 
